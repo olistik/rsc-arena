b613dc9af8da7e4b59d066f8a5d424c6d0a91cf8
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2012-04-26 21:28:00 +0200

    use new Players interface in Sinetris player

--- players/sinetris.rb
+++ players/sinetris.rb
@@ -1,10 +1,15 @@
 module Rsc::Arena::Players
   class Sinetris
     def see_opponent_card(opponent, card)
+      puts "#{opponent} has #{card.value}"
     end
 
     def ask_for_bet(turn_actions)
-      1
+      0
+    end
+
+    def notify_ante_payed(amount)
+      puts "I've payed #{amount}"
     end
     attr_accessor :chips
   end
