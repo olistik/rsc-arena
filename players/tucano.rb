e3e12670fb11387ab30671345e3d31aaf19b04d6
Davide <davide.rambaldi@gmail.com>
2012-04-26 20:57:21 +0200

    tucnao player

---
+++ players/tucano.rb
@@ -0,0 +1,41 @@
+module Rsc::Arena::Players
+  # michele@sportboom.it
+  # sergio.berisso@gmail.com
+  # davide.rambaldi@gmal.com
+  class Tucano    
+    
+    def initialize
+      @max_card = 0
+      @i = 0
+    end
+    
+    attr_accessor :chips
+    
+    def see_opponent_card(opponent, card)
+      
+      #puts "I have #{@chips} chips"
+      #puts "#{card.value.class} , #{@max_card.class}"
+      if (card.value.to_i > @max_card) then @max_card = card.value.to_i end
+      #puts "my opponent #{opponent}, have card #{card.value}"
+    end
+    
+    def ask_for_bet(turn_actions)
+      # strategia di betting
+      #puts "card max: #{@max_card}"
+      
+      if (@i == 0) then 
+        bet = (rand * 10).to_i
+      else        
+        bet = 10 - @max_card
+      end
+      
+      # routine mi gioco tutto
+      if @max_card == 1 then bet = @chips end
+      
+      @max_card = 0
+      @i = @i + 1
+      return bet
+    end
+    
+  end
+end
