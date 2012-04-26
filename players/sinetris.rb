7947cf7cdc659e60a30c64e1ba6befcdd4d5dd69
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2012-04-26 21:19:03 +0200

    Added Sinetris player

---
+++ players/sinetris.rb
@@ -0,0 +1,11 @@
+module Rsc::Arena::Players
+  class Sinetris
+    def see_opponent_card(opponent, card)
+    end
+
+    def ask_for_bet(turn_actions)
+      1
+    end
+    attr_accessor :chips
+  end
+end
