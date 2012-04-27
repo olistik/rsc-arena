0385cf6025de715dda45889277175d8e37da3714
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2012-04-27 02:37:43 +0200

    Sinetris player
    implementation commented until server works as expected

--- players/sinetris.rb
+++ players/sinetris.rb
@@ -1,16 +1,32 @@
 module Rsc::Arena::Players
   class Sinetris
+    attr_accessor :chips
+
+    def initialize
+      @cards = []
+      @round = 0
+    end
+
     def see_opponent_card(opponent, card)
-      puts "#{opponent} has #{card.value}"
+      @cards[@round] = [] unless @cards[@round]
+      @cards[@round] << card
     end
 
     def ask_for_bet(turn_actions)
-      0
+      bet = 0
+      # max_bet = turn_actions.max { |turn_a, turn_b| turn_a[1].to_i <=> turn_b[1].to_i}
+      # unless @cards[@round].nil?
+      #   max = @cards[@round].max { |a, b| a.value.to_i <=> b.value.to_i }.value.to_i
+      #   bet = (@chips * 0.5).floor if max < 6
+      # end
+      # bet = max_bet[1] if max_bet && bet < max_bet[1]
+      # bet = nil if bet > @chips
+      @round += 1
+      return bet
     end
 
     def notify_ante_payed(amount)
-      puts "I've payed #{amount}"
+      puts "Sinetris payed #{amount}"
     end
-    attr_accessor :chips
   end
 end
